705420235f4a7620fd4e0b7e8bfffe31e1b9e6e2
JEEVA300 <124563525+JEEVA300@users.noreply.github.com>
2023-02-15 21:57:44 +0530

    Create Second Largest number among N numbers.c

---
+++ Second Largest number among N numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[1000],i,j,k=0,s[100];
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+      for(j=i+1;j<n;j++)
+      {   
+          if(a[i]>a[j])
+          {
+              t=a[i]
+              a[i]=a[j];
+              a[j]=t;
+           }
+      }
+    }
+    printf("%d",s[k-2]);
+    return 0;
+}
